8c94587c4575983e187b25a63f019e6975a19233
tamphamdev <tampham.dev@gmail.com>
2024-05-02 23:08:24 +0700

    Distributive conditional types

--- src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -2,6 +2,13 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Fruit = "apple" | "banana" | "orange";
 
-type AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
+// type GetAppleOrBanana<T> = T extends "apple" | "banana" ? T : never
+// type AppleOrBanana =  GetAppleOrBanana<Fruit>
+// or
+type AppleOrBanana = Fruit extends infer T
+  ? T extends "apple" | "banana"
+    ? T
+    : never
+  : never;
 
 type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
