0855d0e2bdfa6fb70506cecf007fc7e0457a5ce0
tamphamdev <tampham.dev@gmail.com>
2024-06-09 14:43:26 +0700

    Implement get dynamic path params

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,11 +1,12 @@
 import { Equal, Expect } from "../helpers/type-utils";
-
+import { S } from "ts-toolbelt";
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
-
+type ExtractPathParams<Path extends string> = {
+  [K in S.Split<Path, "/">[number] as K extends `:${infer P}` ? P : never]: string
+}
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
   Expect<
